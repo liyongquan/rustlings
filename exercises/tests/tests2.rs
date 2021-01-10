392a4f02bbdae5f5b981cc7b4a0b45b21afd82f4
liyongquan <496326100@qq.com>
2021-01-10 22:04:10 +0800

    测试框架

--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -2,12 +2,15 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests2` for hints :)
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        //类型不一致是不会相等的
+        //let expect: u32 = 4;
+        let expect: i32 = 4;
+        let actual: i32 = 2 + 2;
+        assert_eq!(expect, actual);
     }
 }
